926c96328315e1597a4c58247f172f33ac6c1cae
zahir <ansanamal999@gmail.com>
2022-12-15 16:26:24 +0500

    my first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+
+var products = [ 
+        {"Id":0 , "Name":"","Brand":"","Price":"","Description":"","Image":"https://st4.depositphotos.com/14953852/24787/v/1600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"} ,
+
+        {"Id":1 , "Name":"Samsung Galaxy A33","Brand":"Samsung","Price":"62000","Description":"NFC (region dependent), Glass front + Gorilla Glass 5, plastic frame, plastic back, plastic back, IP67 dust/water resistant (up to 1m for 30 mins), Photo/video editor, Document viewer ","Image":"Images/1.jpg"} ,
+        
+        {"Id":2 , "Name":"Realme 9","Brand":"Real ME","Price":"63000","Description":"	Active noise cancellation with dedicated mic, Document viewer, Photo/video editor  ","Image":"https://www.whatmobile.com.pk/admin/images/Realme/Realme9-b.jpg"} ,
+        
+        {"Id":3 , "Name":"Infinix Zero 20","Brand":"Infinix","Price":"57000","Description":"Accelerometer, Compass, Fingerprint (side-mounted), Gyro, Proximity ","Image":"https://www.whatmobile.com.pk/admin/images/Infinix/InfinixZero20-b.jpg"} ,
+        
+        
+        {"Id":4 , "Name":"Vivo Y55","Brand":"Vivo","Price":"58000","Description":"Night, Portrait, Photo, Double Exposure, Live Photo, Panorama, Slow Motion, Time-Lapse, Pro, Video","Image":"https://www.whatmobile.com.pk/admin/images/Vivo/VivoY55-b.jpg"} ,
+        
+        {"Id":5, "Name":"Xiaomi","Brand":"RedMe","Price":"57500","Description":"Octa-core (2 x 2.2 GHz Cortex-A76 + 6 x 2.0 GHz Cortex-A55)","Image":"https://www.whatmobile.com.pk/admin/images/Xiaomi/XiaomiRedmiPad-b.jpg"} ,
+        
+        {"Id":6, "Name":"Infinix Hot 12 Play 128GB","Brand":"Infinix","Price":"30000","Description":"	SIM1: GSM 850 / 900 / 1800 / 1900 SIM2: GSM 850 / 900 / 1800 / 1900","Image":"https://www.whatmobile.com.pk/admin/images/Infinix/InfinixHot12Play128GB-b.jpg"} ,
+        
+        {"Id":7, "Name":"QMobile Noir E8","Brand":"QMobile","Price":"27500","Description":"	SLTE band 1(2100), 3(1800), 7(2600), 20(800), 38(2600), 39(1900), 40(2300), 41(2500)  ","Image":"https://www.whatmobile.com.pk/admin/images/Qmobile/NoirE8b.jpg"} ,
+        
+        {"Id":8, "Name":"Apple iPhone 11 Pro","Brand":"IPhone","Price":"One Kydney","Description":"	LTE band 1(2100), 2(1900), 3(1800), 4(1700/2100), 5(850), 7(2600), 8(900), 12(700), 13(700), 17(700), 18(800), 19(800), 20(800), 25(1900), 26(850), 28(700), 29(700), 30(2300), 34(2000), 38(2600), 39(1900), 40(2300), 41(2500), 66(1700/2100)  ","Image":"https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhone11Pro-b.jpg"} ,
+        
+        {"Id":9, "Name":"TecnoCamon 19 Pro Mondrian","Brand":"Techno","Price":"52000","Description":"LTE band 1(2100), 2(1900), 3(1800), 4(1700/2100), 5(850), 7(2600), 8(900), 20(800), 28(700), 38(2600), 40(2300), 41(2500)","Image":"https://www.whatmobile.com.pk/admin/images/Tecno/TecnoCamon19ProMondrian-b.jpg"} ,
+        
+        {"Id":10, "Name":"itel A47","Brand":"Itel","Price":"16000","Description":"Geo-tagging, Phase detection, Night Mode, Video stabilization, Snapshot in video recording, Video (1080p@30fps)","Image":"https://www.whatmobile.com.pk/admin/images/itel/itelA47-b.jpg"}
+        
+        ]
